34550fc2b13188f79130cb3dfe34ddf497177bcb
sarah <sarahhadjhassine@gmail.com>
2020-12-18 20:40:47 +0100

    first commit

---
+++ src/main/java/tn/sarra/easily_stock/EasilyStockApplication.java
@@ -0,0 +1,13 @@
+package tn.sarra.easily_stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasilyStockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasilyStockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/sarra/easily_stock/EasilyStockApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.sarra.easily_stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasilyStockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
